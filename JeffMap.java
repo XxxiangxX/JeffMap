4a7c9032a12a13859c01c2ef875648e28c489541
XxxiangxX <101486441+XxxiangxX@users.noreply.github.com>
2023-10-21 20:57:29 +0800

    Update JeffMap.java

--- JeffMap.java
+++ JeffMap.java
@@ -1,20 +1,23 @@
 import java.util.*;
 
-public final class JeffMap<Key,Objects>{
+public final class JeffMap<Key,Objects> extends HashMap<Key,Objects>{
     private Map<Key,Objects> map;
     private List<Long> code;
     private boolean empty;
+    private final List<Key> c_key = new ArrayList<>();
+    private final List<Objects> c_value = new ArrayList<>();
+    private final JeffCode jeffCode = new JeffCode();
     private final Random random = new Random();
     public JeffMap(){
         try{
-            map = new HashMap<Key, Objects>();
+            map = new HashMap<>();
             code = new ArrayList<>();
         }catch (Exception e){
             e.printStackTrace();
         }
         check();
     }
-    public void check(){
+    private void check(){
         if(map == null){
             try{
                 throw new Exception("NO INSTALLER IN JEFF-MAP!");
@@ -25,27 +28,66 @@ public final class JeffMap<Key,Objects>{
             empty = true;
         }
     }
-    public void add(Key key,Objects object){
+    public void add(final Key key,final Objects object) {
         check();
-        map.put(key,object);
+        map.put(key, object);
+        c_key.add(key);
+        c_value.add(object);
         empty = false;
+    }
+    public void removeFromKey(final Key key){
+        check();
+        map.remove(key);
+        empty = false;
+    }
+    public Boolean containKey(final Key key){
+        for(Key key1 : c_key){
+            if(key == key1){
+                return true;
+            }
+        }
+        return false;
+    }
+    public Boolean containObjects(final Objects value){
+        for(Objects objects : c_value){
+            if(objects == value){
+                return true;
+            }
+        }
+        return false;
     }
     public void print(boolean checkCode){
         if(map.size() != 0){
             if(checkCode){
-                long code;
-                code = random.nextLong();
-                System.out.println((String) map.toString() + " checkCode: " + code);
-                this.code.add(code);
+                System.out.println((String) map.toString() + " checkCode: " + jeffCode.jeffCode());
+                this.code.add(jeffCode.jeffCode());
             }else{
                 System.out.println((String) map.toString());
+            }
+        }
+    }
+    public void printRandom(boolean checkCode){
+        int randomNumber;
+        if(map.size() != 0){
+            if(checkCode){
+                long code;
+                code = random.nextLong();
+                randomNumber = random.nextInt(map.size());
+                System.out.println(map.values().toArray()[randomNumber] + " checkCode: " + jeffCode.jeffCode());
+                this.code.add(jeffCode.jeffCode());
+            }else{
+                randomNumber = random.nextInt(map.size());
+                System.out.println(map.values().toArray()[randomNumber]);
             }
         }
     }
     public List<Long> getCode(){
         return code;
     }
+
+    @Override
     public boolean isEmpty(){
         return empty;
     }
+
 }
